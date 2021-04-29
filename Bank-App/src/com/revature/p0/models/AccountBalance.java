a05d755e771515b565ce69170700b18b93062426
jbialon32 <jbialon32@gmail.com>
2021-04-29 11:18:50 -0500

    Fleshed out AccountBalance and added AccountTransaction

--- Bank-App/src/com/revature/p0/models/AccountBalance.java
+++ Bank-App/src/com/revature/p0/models/AccountBalance.java
@@ -8,4 +8,27 @@ package com.revature.p0.models;
  * Description: {Insert Description}
  */
 public class AccountBalance {
+    private int acctID;
+    private double balance;
+
+    public AccountBalance(int acctID, double balance) {
+        this.acctID = acctID;
+        this.balance = balance;
+    }
+
+    public int getAcctID() {
+        return acctID;
+    }
+
+    public void setAcctID(int acctID) {
+        this.acctID = acctID;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
---
+++ Bank-App/src/com/revature/p0/models/AccountTransaction.java
@@ -0,0 +1,49 @@
+package com.revature.p0.models;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jbialon
+ * Date: 4/29/2021
+ * Time: 11:09 AM
+ * Description: {Insert Description}
+ */
+public class AccountTransaction {
+    private int transactionID;
+    private int acctID;
+    private double transactionAmt;
+
+    public AccountTransaction(int transactionID, int acctID, double transactionAmt) {
+        this.transactionID = transactionID;
+        this.acctID = acctID;
+        this.transactionAmt = transactionAmt;
+    }
+
+    public AccountTransaction(int acctID, double transactionAmt) {
+        this.acctID = acctID;
+        this.transactionAmt = transactionAmt;
+    }
+
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(int transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public int getAcctID() {
+        return acctID;
+    }
+
+    public void setAcctID(int acctID) {
+        this.acctID = acctID;
+    }
+
+    public double getTransactionAmt() {
+        return transactionAmt;
+    }
+
+    public void setTransactionAmt(int transactionAmt) {
+        this.transactionAmt = transactionAmt;
+    }
+}
