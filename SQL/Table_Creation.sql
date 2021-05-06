9bb15ba521ac9d69e22c87399b15b019dabab50c
jbialon32 <jbialon32@gmail.com>
2021-05-06 11:28:11 -0500

    Added check constraint to account_balance table creation script

--- SQL/Table_Creation.sql
+++ SQL/Table_Creation.sql
@@ -33,7 +33,7 @@ create table Account (
 
 create table Account_Balance (
 	Account_Id int primary key references Account (Id),
-	Balance float not null
+	Balance float not null check (Balance > -1)
 );
 
 create table Account_Transaction (
