840487ac246c7170d48685f9f1eb7cc46cd45e98
jbialon32 <jbialon32@gmail.com>
2021-05-06 10:20:27 -0500

    Layed out some required classes and added SQL folder with ERD and table creation

---
+++ Bank-App/src/main/java/com/revature/p0/screens/Screen.java
@@ -0,0 +1,9 @@
+package com.revature.p0.screens;/**
+ * Created by IntelliJ IDEA.
+ * User: Jbialon
+ * Date: 5/5/2021
+ * Time: 9:06 AM
+ * Description: {Insert Description}
+ */
+ public class Screen {
+}
---
+++ SQL/Table_Creation.sql
@@ -0,0 +1,54 @@
+create table User_Table (
+	Id serial primary key,
+	First_Name varchar ( 50 ) not null,
+	Last_Name varchar ( 50 ) not null,
+	Username varchar ( 15 ) unique not null,
+	Email varchar ( 50 ) unique not null,
+	Password varchar ( 72 ) not null
+);
+
+create table Account_Type (
+	Id serial primary key,
+	Acct_Type varchar ( 20 ) unique not null,
+	Interest float not null,
+	Monthly_Fee float not null
+);
+
+create table Account (
+	Id serial primary key,
+	User_Id int not null,
+	Joint_User_Id int,
+	Type_Id int not null,
+	foreign key (User_Id)
+		references User_Table (Id),
+	foreign key (Joint_User_Id)
+		references User_Table (Id),
+	foreign key (Type_Id)
+		references Account_Type (Id)
+);
+
+create table Account_Balance (
+	Account_Id serial primary key references Account (Id),
+	Balance float not null
+);
+
+create table Account_Transaction (
+	Id serial primary key,
+	Account_Id int not null,
+	Transaction_Amt float not null,
+	Description text not null,
+	foreign key (Account_Id)
+		references Account (Id)
+);
+
+create table Transfer (
+	Id serial primary key,
+	Sender int not null,
+	Reciever int not null,
+	Amount float not null,
+	foreign key (Sender)
+		references Account (Id),
+	foreign key (Reciever)
+		references Account (Id)
+);
+
