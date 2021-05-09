eed276f1bebcd2b74830dcc1a4934c3538ada7cd
wynne davis <james.bialon@revature.net>
2021-05-09 14:52:15 -0500

    More work on AccountScreen

--- Bank-App/src/main/java/com/revature/p0/screens/AccountScreen.java
+++ Bank-App/src/main/java/com/revature/p0/screens/AccountScreen.java
@@ -1,5 +1,6 @@
 package com.revature.p0.screens;
 
+import com.revature.p0.daos.AccountBalanceDAO;
 import com.revature.p0.daos.AccountDAO;
 import com.revature.p0.daos.UserDAO;
 import com.revature.p0.models.account.Account;
@@ -44,13 +45,52 @@ public class AccountScreen extends Screen {
 
             while (Integer.parseInt(userSelection) < 0 || Integer.parseInt(userSelection) > userAccounts.length) {
 
+                System.out.println();
+                System.out.println("Invalid selection.");
+                System.out.println();
                 getSelectionScreen(userAccounts);
 
                 userSelection = consoleReader.readLine();
 
             }
 
+            if (userSelection.equals(0)) {
 
+                //TODO: route user to bank account opening screen
+
+            } else {
+
+                System.out.println();
+                showAccountInfo(userAccounts[Integer.parseInt(userSelection)]);
+
+                userSelection = consoleReader.readLine();
+
+                while (Integer.parseInt(userSelection) < 0 || Integer.parseInt(userSelection) > 3) {
+
+                    System.out.println();
+                    System.out.println("Invalid selection.");
+                    System.out.println();
+                    showAccountInfo(userAccounts[Integer.parseInt(userSelection)]);
+
+                    userSelection = consoleReader.readLine();
+
+                }
+
+                switch (userSelection) {
+                    case "0":
+                        //TODO exit
+
+                    case "1":
+                        //TODO deposit screen
+
+                    case "2":
+                        //TODO withdraw screen
+
+                    case "3":
+                        //TODO transaction screen
+                }
+
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -74,5 +114,21 @@ public class AccountScreen extends Screen {
         System.out.println("0 - Open a new account.");
 
         System.out.print("> ");
+    }
+
+    private void showAccountInfo(Account acct) {
+
+        AccountBalanceDAO balanceDAO = new AccountBalanceDAO();
+
+        System.out.printf("Account Name: %s\nBalance: %f", acct.getaName(), balanceDAO.getBalance(acct));
+        System.out.println("");
+
+        System.out.println("1 - Deposit");
+        System.out.println("2 - Withdraw");
+        System.out.println("3 - Transaction History");
+        System.out.println("0 - Exit");
+
+        System.out.print("> ");
+
     }
 }
